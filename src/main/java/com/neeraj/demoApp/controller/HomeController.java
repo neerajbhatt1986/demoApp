0ff0a5a82aa3fe4f2b5a9997b53b5c6bec865c85
Niraj Bhatt <niraj.bhatt@synechron.com>
2019-06-19 09:53:42 +0530

    rebuilding

--- src/main/java/com/neeraj/demoApp/controller/HomeController.java
+++ src/main/java/com/neeraj/demoApp/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
+
     @GetMapping("home")
     String getHome(){
         return "hello";
